c56a25b691c8e36000018b6372c21485c6c551ce
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-11-10 15:28:41 -0500

    Update first.js

--- first.js
+++ first.js
@@ -1,4 +1,12 @@
 // comments
 // parameters: variables that can Change 
-
-console.log(2+2);
+var startingNumber = 1;
+var currentNumber = startingNumber;
+var endNumber = 100;
+var sum = 0;
+// add all the numbers from 1 to 100
+for ( ; currentNumber <= endNumber  ; ){
+    currentNumber = currentNumber + 1;
+    sum = sum + currentNumber;
+}
+console.log(sum);
