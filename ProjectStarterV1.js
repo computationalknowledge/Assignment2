e336ad05e05cd9052917b6ed7451b47ced4a0a6e
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-11-20 10:22:32 -0500

    Update ProjectStarterV1.js

--- ProjectStarterV1.js
+++ ProjectStarterV1.js
@@ -2,11 +2,13 @@ var sqlite3 = require('sqlite3');
 var http = require("http");
 
 var file = "CIS.db";
-
+var a = null;
+var b = null; 
 var db = new sqlite3.Database(file);
 db.all("SELECT * from students", function(err, rows) {
     rows.forEach(function(row) {
-        console.log(row.studentname, row.studentid);
+        a = row.studentname;
+        b = row.studentid;
     })
 });
 db.close();
@@ -14,6 +16,6 @@ db.close();
 http.createServer(function(request, response) {
     console.log("answering an incoming request");
 response.writeHead(200,  {"Content-Type":  "text/plain"});
-response.write("Hello NOV 20 ");
-response.end();
+response.write("name: " + a);
+response.end(" ID: " + b);
 }).listen(8880);
